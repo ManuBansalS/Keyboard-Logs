07dbb0d1c4c840eee7f032d028357ce1a5350ad1
Manu Bansal <manu03bansal@gmail.com>
2025-07-25 00:31:30 +0530

    uploading src

---
+++ src/script/script.js
@@ -0,0 +1,30 @@
+const insert  = document.getElementById('insert')
+let keyHistory = []
+
+function renderData(){
+    insert.innerHTML = 
+    `
+    Press any Key and see the Magic
+    <div class = "table">
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>KeyCode</th>
+                <th>Code</th>
+            </tr>
+            ${keyHistory.map(event => `
+                    <tr>
+                        <td>${event.key === " " ? "Space" : event.key}</td>
+                        <td>${event.keyCode}</td>
+                        <td>${event.code}</td>
+                    </tr>
+                `).join('')}
+        </table>
+    </div>
+    `
+}
+
+window.addEventListener('keydown', (e) => {
+    keyHistory.push({key: e.key, keyCode: e.keyCode, code: e.code})
+    renderData()
+})
